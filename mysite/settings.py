8b8a06f127b0b6979fbc9d4b02831d0d948c31b3
jokecloud <medalhaemerson@gmail.com>
2022-09-30 15:13:20 -0300

    blog mail

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,3 +125,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email server configuration
+"""
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'falsoncourse@gmail.com'
+EMAIL_HOST_PASSWORD = 'pass'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+"""
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
